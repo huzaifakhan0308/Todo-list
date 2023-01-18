a55c06cc776e9ec00f8e2fa324d6baec760abaa9
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-01-18 23:26:08 +0500

    fix linters errors

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './style.css';
 
 class NewList {
   constructor(NewValue, index) {
-    this.completed = true;
     this.inputValue = NewValue;
     this.indexNumber = index;
+    this.completed = true;
   }
 }
 

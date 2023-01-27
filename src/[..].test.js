26e16c40f530ecdac94f79d5539130537e8e88a9
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-01-28 00:04:39 +0500

    create file for testing

--- src/[..].test.js
+++ src/[..].test.js
@@ -1,43 +1,39 @@
-import Abc from './modules/newList.js';
+import NewList from './modules/newList.js';
 
-// class LocalStorageMock {
-//   constructor() {
-//     this.store = {};
-//   }
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
 
-//   clear() {
-//     this.store = {};
-//   }
+  clear() {
+    this.store = {};
+  }
 
-//   getItem(key) {
-//     return this.store[key] || null;
-//   }
+  getItem(key) {
+    return this.store[key] || null;
+  }
 
-//   setItem(key, value) {
-//     this.store[key] = String(value);
-//   }
+  setItem(key, value) {
+    this.store[key] = String(value);
+  }
 
-//   removeItem(key) {
-//     delete this.store[key];
-//   }
-// }
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
 
-// global.localStorage = new LocalStorageMock();
+global.localStorage = new LocalStorageMock();
 
-const functions = new Abc();
 const obj1 = { value: 'hello', completed: false, index: null };
-const obj2 = { value: 'world', completed: false, index: null };
+
+const functions = new NewList();
 
 describe('functionality methods', () => {
-  test('Add new task', () => {
+  it('Add new task', () => {
     expect(functions.add(obj1)).toStrictEqual([{ value: 'hello', completed: false, index: 1 }]);
   });
 
-  test('Add new task', () => {
-    expect(functions.add(obj2)).toStrictEqual([{ value: 'hello', completed: false, index: 1 }, { value: 'world', completed: false, index: 2 }]);
-  });
-
-  test('remove a task', () => {
-    expect(functions.remove(2)).toStrictEqual([{ value: 'world', completed: false, index: 1 }]);
+  it('remove task', () => {
+    expect(functions.remove(1)).toHaveLength(0);
   });
 });
